ba082dbf8ebd969e3f1f0e5affb35bb488ecc593
José Á. Calero M <joseacalerom@gmail.com>
2023-12-07 00:53:58 +0100

    Exercise Components - 4 &  Comment Fixed
    Comments Exercise Components - 3 fixed
    Change Vite's default port to 3000

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,9 +4,6 @@ import { Message } from "./Message";
 export function App() {
   return (
     <div>
-    //We can use the Hello component more than once in our App component
-    //We have no problems with that
-    //The same way we can render the Message component and we can show its content
       <Hello />
       <Hello />
       <Hello />
@@ -14,3 +11,7 @@ export function App() {
     </div>
   );
 }
+
+    //We can use the Hello component more than once in our App component
+    //We have no problems with that
+    //The same way we can render the Message component and we can show its content
