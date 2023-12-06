4e0f5fd81f02f976ad8caf924c524c5467a33ad6
José Á. Calero M <joseacalerom@gmail.com>
2023-12-07 00:46:41 +0100

    Exercise Components - 3
    Adding Hello and Message components into App component more than once

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,9 +1,16 @@
 import { Hello } from "./Hello";
+import { Message } from "./Message";
 
 export function App() {
   return (
     <div>
+    //We can use the Hello component more than once in our App component
+    //We have no problems with that
+    //The same way we can render the Message component and we can show its content
       <Hello />
+      <Hello />
+      <Hello />
+      <Message />
     </div>
   );
 }
