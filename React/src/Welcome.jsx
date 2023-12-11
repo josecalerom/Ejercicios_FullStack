37bdf758f2f7b61fd7d37e18817059c4f00a0d98
José Á. Calero M. <joseacalerom@gmail.com>
2023-12-11 01:25:42 +0100

    Exercise Conditional Rendering - 1

--- React/src/Welcome.jsx
+++ React/src/Welcome.jsx
@@ -4,15 +4,16 @@ export function Welcome({ name="Fulano Pérez", age}) {
     return (
         <>
             <p>Welcome, <strong>{name}</strong>!</p>
-            <Age age={age}/>
+            <h4>Age component #1</h4>
+            <Age age={age} />
+            <h4>Age component #2: age greater than 18</h4>
+            {age > 18 && <Age age={age} />}
+            <h4>Age component #3: age prop is present</h4>
+            {Boolean(age) && <Age age={age} />}
+            <h4>Age component #4: age greater than 18 and less than 65</h4>
+            {age > 18 && age < 65 && <Age age={age} />}
+            <h4>Age component #5: same as the last one but also name equals to "John"</h4>
+            {age > 18 && age < 65 && name==="John" && <Age age={age} />}
         </>
     );
 }
-
-//Q: What happens if no name prop is passed to the Welcome component?
-//A: If no name prop is passed and there's no default value for that prop, it will render an empty string
-//But if there's no explicit value and we have a default name prop value, React will render the default value
-
-//Q: Can you set a default value for the name prop?
-//A: We can set the default value when creating the Welcome component or using the "Welcome.defaultProps" to set the default values
-
