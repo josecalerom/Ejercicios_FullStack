ae7110e7bcb89f184fea348865130ab9d8f8d565
José Á. Calero M. <joseacalerom@gmail.com>
2023-12-11 01:33:32 +0100

    Exercise Conditional Rendering - 2

--- React/src/Age.jsx
+++ React/src/Age.jsx
@@ -1,5 +1,7 @@
 export function Age({ age }) {
     return(
-        <p>Your age is {age}</p>
+        <>
+            {age > 18 ? <p>Your age is {age}</p> : <p>You are very young!</p>}
+        </>
     )
 }
--- React/src/Welcome.jsx
+++ React/src/Welcome.jsx
@@ -4,16 +4,7 @@ export function Welcome({ name="Fulano Pérez", age}) {
     return (
         <>
             <p>Welcome, <strong>{name}</strong>!</p>
-            <h4>Age component #1</h4>
             <Age age={age} />
-            <h4>Age component #2: age greater than 18</h4>
-            {age > 18 && <Age age={age} />}
-            <h4>Age component #3: age prop is present</h4>
-            {Boolean(age) && <Age age={age} />}
-            <h4>Age component #4: age greater than 18 and less than 65</h4>
-            {age > 18 && age < 65 && <Age age={age} />}
-            <h4>Age component #5: same as the last one but also name equals to "John"</h4>
-            {age > 18 && age < 65 && name==="John" && <Age age={age} />}
         </>
     );
 }
