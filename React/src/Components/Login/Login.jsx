b832c735e29a64509e630f2eba94b4130b1e91cc
José Á. Calero M. <joseacalerom@gmail.com>
2023-12-15 01:39:37 +0100

    Exercise 21 - Controlled Forms - 5

--- React/src/Components/Login/Login.jsx
+++ React/src/Components/Login/Login.jsx
@@ -25,16 +25,20 @@ export function Login({ onLogin }) {
     });
   }
 
-  function handleLogin() {
+  function handleLoginFormSubmit(event) {
+    event.preventDefault();
     onLogin(data);
   }
 
+  //Q: How do you prevent the default behavior of the form element?
+  //A: we can use the "event.preventDefault()" to avoid the default behavior of the form element
+
   function handleResetForm() {
-    setData(createData())
+    setData(createData());
   }
 
   return (
-    <div>
+    <form onSubmit={handleLoginFormSubmit}>
       <input
         placeholder="Your name"
         value={data.username}
@@ -55,10 +59,8 @@ export function Login({ onLogin }) {
         type="checkbox"
         onChange={handleInputChange}
       />
-      <button disabled={!data.password || !data.username} onClick={handleLogin}>
-        Login
-      </button>
+      <button disabled={!data.password || !data.username}>Login</button>
       <button onClick={handleResetForm}>Reset</button>
-    </div>
+    </form>
   );
 }
