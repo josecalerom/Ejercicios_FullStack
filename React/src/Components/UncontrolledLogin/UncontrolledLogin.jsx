7443035f1bfa31c332bfad03d53c3121d308de7c
José Á. Calero M. <joseacalerom@gmail.com>
2023-12-16 03:56:05 +0100

    Exercise 23 - Uncontrolled Forms - 2

--- React/src/Components/UncontrolledLogin/UncontrolledLogin.jsx
+++ React/src/Components/UncontrolledLogin/UncontrolledLogin.jsx
@@ -2,6 +2,8 @@ export function UncontrolledLogin() {
     function handleFormSubmit(event) {
         event.preventDefault();
 
+    
+    //Accessing values of the form with FormData API
     const formData = new FormData(event.target);
 
     const data = {
@@ -9,6 +11,18 @@ export function UncontrolledLogin() {
       password: formData.get("password"),
       remember: formData.get("remember") === "on" ? true : false,
     };
+
+
+    //Accessing values of the form without FormData API
+    // const username = event.target.elements.namedItem("username").value;
+    // const password = event.target.elements.namedItem("password").value;
+    // const remember = event.target.elements.namedItem("remember").checked;
+    // const data = {username, password, remember}
+
+    //Attempt to access the values of the form by using the FormData API.
+    //Q: What are the advantages? What are the disadvantages?
+    //A: It provides a way to easily store key-value pairs in relation to HTML form fields and values
+    //However older browsers do not support this feature
 
     console.log(data);
   }
