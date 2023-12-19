88394270a676e78f13caed757c5c54c4180c3952
José Á. Calero M. <joseacalerom@gmail.com>
2023-12-19 19:16:51 +0100

    Exercise 28 - Lists and State - 1

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,12 +1,4 @@
-import { AlertClock } from "./Components/AlertClock/AlertClock";
-import { FocusableInput } from "./Components/FocusableInput/FocusableInput";
-import { Hello } from "./Components/Hello/Hello";
-import { Counter } from "./Components/Counter/Counter"
-import { Login } from "./Components/Login/Login";
-import { UncontrolledLogin } from "./Components/UncontrolledLogin/UncontrolledLogin";
-import { StrictmodeFocusableInput } from "./Components/StrictmodeFocusableInput/StrictmodeFocusableInput";
-import { Color } from "./Components/Color/Color"
-import { Colors } from "./Components/Colors/Colors"
+import { TodoList } from "./Components/TodoList/TodoList";
 
 export function App() {
 
@@ -21,18 +13,8 @@ export function App() {
 
   return (
     <div>
-      <h3>This is the Color component</h3>
-      <Color color={{name: "José", id: 3456}}/>
-      <h3>This is the Colors component</h3>
-      <Colors colors={[
-        {name: "Julio", id: 1234},
-        {name: "Mario", id: 5678},
-        {name: "Ana", id: 91011},
-        {name: "Gabriela", id: 121314},
-        {name: "Camilo", id: 151617},
-        {name: "Eduardo", id: 181920},
-        {name: "Maite", id: 212223},
-      ]}/>
+      <h3>This is the TodoList component</h3>
+      <TodoList />
     </div>
   );
 }
---
+++ React/src/Components/TodoList/TodoList.jsx
@@ -0,0 +1,31 @@
+import { useState, useRef, useEffect } from "react"
+
+export function TodoList() {
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current?.focus()
+      }, [])
+
+    const [todos, setTodos] = useState([])
+    const [todoInput, setTodoInput] = useState("")
+
+    const handleSubmitTodo = () => {
+        if(todoInput.trim() !== ""){
+        setTodos([...todos, todoInput])
+        setTodoInput("")
+    }
+}
+    const handleInputChange = (e) => setTodoInput(e.target.value)
+
+    return (
+        <>
+        <input ref={inputRef} type="text" value={todoInput} onChange={handleInputChange}/>
+        <button onClick={handleSubmitTodo}>Add Todo</button>
+        <ul>
+            {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+        </ul>
+        </>
+    )
+}
