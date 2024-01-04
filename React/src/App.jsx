44ceecc3df857f2bcbc52eefd5b417c769f17207
José Á. Calero M. <joseacalerom@gmail.com>
2024-01-04 02:44:48 +0100

    Exercise 48 - React Router - 2

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,4 +1,5 @@
 import { Welcome } from "./components/Welcome/Welcome.jsx";
+import { Counter } from "./Components/Counter/Counter.jsx"
 import { Route, Routes } from "react-router-dom";
 
 export function App() {
@@ -6,6 +7,7 @@ export function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome name="José Ángel" />} />
+      <Route path="/counter" element={<Counter initialValue={0} difAmount={5} />}/>
     </Routes>
   );
 }
