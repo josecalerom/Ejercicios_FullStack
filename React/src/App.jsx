1b660bf6bce6227ce2b83601d99829bdd5d26d74
José Á. Calero M. <joseacalerom@gmail.com>
2024-01-04 20:28:34 +0100

    Exercise 50 - React Router - 4

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,15 +1,21 @@
-import { Welcome } from "./components/Welcome/Welcome.jsx";
+import { Welcome } from "./components/Welcome/Welcome.jsx"
 import { Counter } from "./Components/Counter/Counter.jsx"
 import ShowGitHubUser from "./Components/ShowGitHubUser/ShowGitHubUser.jsx"
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 
 export function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Welcome name="José Ángel" />} />
-      <Route path="/counter" element={<Counter initialValue={0} difAmount={5} />}/>
-      <Route path="/users/:username" element={<ShowGitHubUser />} />
-    </Routes>
+    <>
+    <nav>
+      <Link to="/">Home page</Link> | <Link to="/users/josecalerom">GitHub user</Link> | <Link to="/counter">Counter</Link>
+      <hr />
+    </nav>
+      <Routes>
+        <Route path="/" element={<Welcome name="José Ángel" />} />
+        <Route path="/counter" element={<Counter initialValue={0} difAmount={5} />}/>
+        <Route path="/users/:username" element={<ShowGitHubUser />} />
+      </Routes>
+    </>
   );
 }
