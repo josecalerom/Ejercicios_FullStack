44bfdd27dc7a121bdbbb3bb0e5efb758b300ebb5
José Á. Calero M. <joseacalerom@gmail.com>
2024-01-05 01:23:34 +0100

    Exercise 55 - SWR - 2

--- React/src/Components/CustomHooks/useGitHubUser.jsx
+++ React/src/Components/CustomHooks/useGitHubUser.jsx
@@ -3,10 +3,13 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function useGithubUser(username) {
+  if(username){
   const { data, error } = useSWR(
     `https://api.github.com/users/${username}`,
     fetcher
   );
+  return {data, error}
+  }
 
   return { data, error, loading: !data && !error };
 }
