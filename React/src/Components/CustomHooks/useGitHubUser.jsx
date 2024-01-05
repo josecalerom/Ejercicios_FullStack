00e52fff639b35210c189ede038912019af392bc
José Á. Calero M. <joseacalerom@gmail.com>
2024-01-05 01:33:42 +0100

    Exercise 57 - SWR - 4

--- React/src/Components/CustomHooks/useGitHubUser.jsx
+++ React/src/Components/CustomHooks/useGitHubUser.jsx
@@ -1,11 +1,8 @@
 import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((response) => response.json());
-
 export default function useGithubUser(username) {
   const { data, error, mutate } = useSWR(
     `https://api.github.com/users/${username}`,
-    fetcher
   );
 
   const refetchGitHubUser = () => {
--- React/src/Root.jsx
+++ React/src/Root.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
+import { SWRConfig } from 'swr';
+
+const fetcher = url => fetch(url).then( response => response.json())
 
 export const Root = () => {
   return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SWRConfig value={{ fetcher }}>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+    </SWRConfig>
   );
 };
