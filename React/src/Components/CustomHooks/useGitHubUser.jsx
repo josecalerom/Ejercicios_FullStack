5ca8f134e1a3f588bface7d72b64749d73332a3a
José Á. Calero M. <joseacalerom@gmail.com>
2024-01-05 01:13:45 +0100

    Exercise 54 - SWR - 1

--- React/src/Components/CustomHooks/useGitHubUser.jsx
+++ React/src/Components/CustomHooks/useGitHubUser.jsx
@@ -1,36 +1,12 @@
-import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function useGithubUser(username) {
+  const { data, error } = useSWR(
+    `https://api.github.com/users/${username}`,
+    fetcher
+  );
 
-const [data, setData] = useState(null);
-const [loading, setLoading] = useState(false);
-const [error, setError]= useState(null)
-
-  async function fetchGitHubUser(username) {
-    try {
-      setLoading(true)
-      setError(null)
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      const json = await response.json();
-      console.log(json);
-
-      if(response.status !== 200) {
-        setError(new Error())
-      }
-
-      setData(json);
-    } catch (error) {
-      setError(error);
-      setData(null);
-      console.log("Error", error);
-    } finally {
-        setLoading(false);
-    }
-  }
-  useEffect(() => {
-    fetchGitHubUser(username);
-  }, [username]);
-
-  return { data , loading , error };
-
+  return { data, error, loading: !data && !error };
 }
