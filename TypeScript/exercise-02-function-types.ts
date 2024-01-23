6b33ce1831e64c6f1955560d897b1daa63a49d79
José Á. Calero M. <joseacalerom@gmail.com>
2024-01-23 23:41:21 +0100

    Ejercicios TypeScript 02-05

--- TypeScript/exercise-02-function-types.ts
+++ TypeScript/exercise-02-function-types.ts
@@ -4,7 +4,7 @@
 
 // Add types to this function declaration.
 
-function doubleThePopulation(value) {
+function doubleThePopulation(value:number):number {
     return value * 2;
 }
 
@@ -12,21 +12,23 @@ function doubleThePopulation(value) {
 
 doubleThePopulation(5);
 
-doubleThePopulation("6.12");
+doubleThePopulation(6.12);
 
 doubleThePopulation(8_526);
 
-doubleThePopulation(false);
+// Remove: doubleThePopulation(false);
 
 // Alter this function so the `language2` parameter is optional.
 // Hint: Check `language2` is not `undefined` before passing it to `console.log()`.
 
-function languagesSpoken(country: string, language1: string, language2: string): void {
+function languagesSpoken(country: string, language1: string, language2?: string): void {
     console.log(`The languages spoken in ${country} are:`);
 
     console.log(language1);
 
+    if(language2 !== undefined){
     console.log(language2);
+    }
 }
 
 languagesSpoken("Colombia", "Spanish", "English");
